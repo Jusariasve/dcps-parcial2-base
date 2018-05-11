250611828b9d9a64ce5d3aa7a4a47895c91a6396
Juan Sebastián Arias <jusariasve@unal.edu.co>
2018-05-11 11:50:03 -0500

    Correción de error

--- app/controllers/authors/author.js
+++ app/controllers/authors/author.js
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  msgDelete: null;
-
   actions:{
     deletePost(blogPost){
       let answer = confirm('Do you want delete this post?');
